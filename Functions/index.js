95c6b778dd660e43038ae020529258ca3030ebaf
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-22 12:39:47 -0600

    Initial Commit

---
+++ Functions/index.js
@@ -0,0 +1,14 @@
+// Performing a task
+function greet(name, lastName) {
+
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+greet('Sanjith', 'Manne');
+
+console.log(square(2));
