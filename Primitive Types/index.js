928440027b4e82a642659ac9bc79744ef5cd2e68
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-22 12:07:09 -0600

    Initial Commit

---
+++ Primitive Types/index.js
@@ -0,0 +1,5 @@
+let name = 'Mosh'; //string literal
+let age = 30; //number literal
+let isApproved = true; //boolean literal (true/false)
+let firstName = undefined; //undefined
+let selectedColor = null; //used when we want to clear the value of a variable
