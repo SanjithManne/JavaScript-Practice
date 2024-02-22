34aa368a52cedb52788fcc1902969d25a7883484
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-22 12:29:09 -0600

    Initial Commit

---
+++ Arrays/index.js
@@ -0,0 +1,10 @@
+let selectedColors = ['red' , 'blue'];
+console.log(selectedColors);
+
+selectedColors[2] = 'grey';
+console.log(selectedColors);
+
+selectedColors[2] = 2;
+console.log(selectedColors);
+
+console.log(selectedColors.length);
