7bd748ee048f68519d63b3d2fad97cac8fd59705
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-22 11:58:53 -0600

    Initial Commit

---
+++ Variables/index.js
@@ -0,0 +1,11 @@
+let name = 'SanjithManne';
+console.log(name);
+
+//Variable name cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number
+//Cannot contain a space or hyphen (-)
+//Variables are case-sensitive
+
+let firstName = 'Sanjith';
+let lastName = 'Manne';
