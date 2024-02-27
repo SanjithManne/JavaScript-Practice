f4b14dffec8ca68ba15b6a4e5c9bb1c111e8de6e
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-02-27 11:21:51 -0600

    Initial Commit

---
+++ Rock Paper Scissors/index.js
@@ -0,0 +1,55 @@
+function rock() {
+    const randomNumber = Math.random();
+    
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+    console.log('Both chose rock!');
+   }
+
+   else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    console.log('The computer chose paper');
+    console.log('You Lose!');
+   }
+
+   else {
+    console.log('The computer chose scissors');
+    console.log('You Win!');
+   }
+}
+
+function paper() {
+    const randomNumber = Math.random();
+
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+        console.log('The computer chose rock');
+        console.log('You Win!');
+       }
+    
+       else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        console.log('Both chose paper!');
+       }
+    
+       else {
+        console.log('The computer chose scissors');
+        console.log('You Lose!');
+       }
+}
+
+function scissors() {
+    const randomNumber = Math.random();
+
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+        console.log('The computer chose rock');
+        console.log('You Lose!');
+       }
+    
+       else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        console.log('The computer chose paper');
+        console.log('You Win!')
+       }
+    
+       else {
+        console.log('Both chose scissors!');
+       }
+}
+
+
