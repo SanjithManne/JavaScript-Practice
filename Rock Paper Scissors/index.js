c07478d7c170fb62339b551639832b58eb4d14b6
SanjithManne <109084240+SanjithManne@users.noreply.github.com>
2024-03-02 15:36:10 -0600

    Update 2

--- Rock Paper Scissors/index.js
+++ Rock Paper Scissors/index.js
@@ -1,58 +1,67 @@
+let computerMove = '';
+let result = '';
+
 function rock() {
     const randomNumber = Math.random();
     
     if (randomNumber >= 0 && randomNumber < 1/3) {
-    console.log('Both chose rock');
-    console.log('Its a Draw!');
+    computerMove ='Both chose rock';
+    result = 'Draw!';
    }
 
    else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-    console.log('The computer chose paper');
-    console.log('You Lose!');
+   computerMove = 'The computer chose paper';
+   result = 'You Lose!';
    }
 
    else {
-    console.log('The computer chose scissors');
-    console.log('You Win!');
+    computerMove = 'The computer chose scissors';
+    result = 'You Win!';
    }
+   
+   alert(computerMove + '\n' + result);
 }
 
 function paper() {
     const randomNumber = Math.random();
 
     if (randomNumber >= 0 && randomNumber < 1/3) {
-        console.log('The computer chose rock');
-        console.log('You Win!');
+        computerMove = 'The computer chose rock';
+        result = 'You Win!';
        }
     
        else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-        console.log('Both chose paper');
-        console.log('Its a Draw!');
+        computerMove = 'Both chose paper';
+        result = 'Draw!';
        }
     
        else {
-        console.log('The computer chose scissors');
-        console.log('You Lose!');
+        computerMove = 'The computer chose scissors';
+        result = 'You Lose!';
        }
+
+       alert(computerMove + '\n' + result);
 }
 
 function scissors() {
     const randomNumber = Math.random();
 
     if (randomNumber >= 0 && randomNumber < 1/3) {
-        console.log('The computer chose rock');
-        console.log('You Lose!');
+        computerMove = 'The computer chose rock';
+        result = 'You Lose!';
        }
     
        else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-        console.log('The computer chose paper');
-        console.log('You Win!')
+        computerMove = 'The computer chose paper';
+        result = 'You Win!';
        }
     
        else {
-        console.log('Both chose scissors!');
-        console.log('Its a Draw!');
+        computerMove = 'Both chose scissors!';
+        result = 'Draw!';
        }
+
+       alert(computerMove + '\n' + result);
 }
 
 
